5789556fbaec2bcf9b50b1e0f2a2a607f17bae44
lynn <152962896+lmkjfm@users.noreply.github.com>
2025-12-07 07:55:05 +0300

    Add files via upload

---
+++ student details assignments.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    char Names [8]="Wanjiku";
+    int Age;
+    float Height;
+    float Weight;
+    char KCSEGrade[1]="B";
+    Age=25;
+    Height=5.2;
+    Weight=67.67;
+    printf("Names: %s\n", Names);
+    printf("Age: %d\n",Age);
+    printf("Height:%.1f\n",Height);
+    printf("Weight:%.2f\n",Weight);
+    printf("KCSE Grade:%.1s", KCSEGrade);
+
+
+
+
+
+}
